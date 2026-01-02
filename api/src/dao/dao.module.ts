4a7319bac6f0348aaf7de35f5ca3a1f125b82788
InnoOkeke <inno.okeke@outlook.com>
2026-01-02 11:17:45 +0100

    fix: updates

--- api/src/dao/dao.module.ts
+++ api/src/dao/dao.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { DaoService } from './dao.service';
 import { DaoController } from './dao.controller';
 import { PrismaService } from '../prisma.service';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+    imports: [AuthModule],
     controllers: [DaoController],
     providers: [DaoService, PrismaService],
 })
--- api/src/investments/investments.module.ts
+++ api/src/investments/investments.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { InvestmentsService } from './investments.service';
 import { InvestmentsController } from './investments.controller';
 import { PrismaService } from '../prisma.service';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [InvestmentsController],
   providers: [InvestmentsService, PrismaService],
 })
